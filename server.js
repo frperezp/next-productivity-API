4dc70899156e11806835a15bd0f48f5f09e5e87f
Ferran Pérez Pastor <>
2024-11-22 18:00:25 +0100

    Arreglando el cors

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const cors = require("cors");
 const app = express();
 const PORT = 5000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
 // Habilita CORS
-app.use(cors());
+app.use(cors({ origin: "http://react-app:3000" }));
 
 app.use(bodyParser.json());
 
